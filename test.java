11ad73fe6c15abf9c4f616dc45db87b183230c75
bhushan-introtuce <bhushan@introtuce.com>
2021-02-25 18:53:29 +0530

    for changes

--- test.java
+++ test.java
@@ -3,7 +3,6 @@ import java.util.*;
 
 class Solution{
 
-
     public static void main(String[] args) throws Exception{
     
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -11,11 +10,21 @@ class Solution{
         String s = br.readLine();
 
         System.out.println(s);
-        
 
-        
+        Map<String ,String> map = new HashMap<>();
 
+        map.put("1", "Bhushan");
+        map.put("2", "Pratiksha");
+        map.put("3", "Kajal");
+        map.put("4", "Pranay");
+        map.put("5", "Kartik");
+
+        for (String string : map.keySet()) {
+
+            System.out.println(map.get(string));
+            
+        }
+        
     
-
     }
 }
