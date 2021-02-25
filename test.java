1f8d3947d2de5a2390a45b7331c01a938bbfae97
bhushan-introtuce <bhushan@introtuce.com>
2021-02-25 18:45:14 +0530

    Firstr commit

---
+++ test.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+
+class Solution{
+
+
+    public static void main(String[] args) throws Exception{
+    
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String s = br.readLine();
+
+        System.out.println(s);
+        
+
+        
+
+    
+
+    }
+}
